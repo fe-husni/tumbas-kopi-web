a0efb7f4d51f85b547e044155419240545cb782d
Husni Mubarok <husniiimubarok00@gmail.com>
2024-02-24 15:52:58 +0700

    commit - shopping cart & search feature, featured product section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
-// Toggle class active
+// Toggle class active Hamburger Menu
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
 
-// Klik outside sidebar untuk close
+// Toggle class active Search Form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active Shopping Cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Klik outside elemen
 const hamburger = document.querySelector('#hamburger-menu');
+const searchBtn = document.querySelector('#search-button');
+const shoppingCartBtn = document.querySelector('#shopping-cart-button');
+
 document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+
+  if (!searchBtn.contains(e.target) && !navbarNav.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!shoppingCartBtn.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
   }
 });
